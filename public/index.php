e6a2d730f3f198ddded04032995b880620088ad5
bouchta65 <bouchtamohamed01@gmail.com>
2024-12-08 22:14:38 +0100

    test : ajout du test de connexion à la base de données

--- public/index.php
+++ public/index.php
@@ -5,7 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>application de gestion de location de voituresapplication de gestion de location de voitures</title>
 </head>
+
 <body>
-    
+<?php include '../src/config/config.php'; ?>
 </body>
+
+
+
 </html>
