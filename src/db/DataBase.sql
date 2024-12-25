d6dd4b73b5e799913dc84179866cc8f3a2f6cf52
bouchta65 <bouchtamohamed01@gmail.com>
2024-12-25 15:08:08 +0100

    update the database

--- src/db/DataBase.sql
+++ src/db/DataBase.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
 -- Host: localhost    Database: locationvoiture
 -- ------------------------------------------------------
@@ -14,6 +14,31 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admin` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admin`
+--
+
+LOCK TABLES `admin` WRITE;
+/*!40000 ALTER TABLE `admin` DISABLE KEYS */;
+INSERT INTO `admin` VALUES (1,'admin@gmail.com','admin');
+/*!40000 ALTER TABLE `admin` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `client`
@@ -39,7 +64,7 @@ CREATE TABLE `client` (
 
 LOCK TABLES `client` WRITE;
 /*!40000 ALTER TABLE `client` DISABLE KEYS */;
-INSERT INTO `client` VALUES ('jf325','Hassani','lhbib','lahbib14@gmail.com.com','0687654321'),('jf415','Bouchta','Mohamed','Bouchtamohamed01@gmail.com','0762389431'),('sf5894','benelaarbi','khadija','khadija2021@gmail.com','0668382431');
+INSERT INTO `client` VALUES ('Jf5698','brahimi','hamid','hamidbrahimi17@gmail.com','0635987465'),('jf6898','alaalawi','ahmed','ahmed2021@gmail.com','0689748596'),('sf5894','benelaarbim','khadija','khadija2021@gmail.com','0668382431'),('SH1256','el Quaul','Saad','saade14@gmail.com','0689465798');
 /*!40000 ALTER TABLE `client` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -57,12 +82,13 @@ CREATE TABLE `contrat` (
   `date_debut` date NOT NULL,
   `date_fin` date NOT NULL,
   `Duree` int NOT NULL,
+  `prixtotal` decimal(10,2) DEFAULT NULL,
   PRIMARY KEY (`contrat_id`),
   KEY `voiture_id` (`voiture_id`),
   KEY `client_id` (`client_id`),
   CONSTRAINT `contrat_ibfk_1` FOREIGN KEY (`voiture_id`) REFERENCES `voiture` (`matricule`) ON DELETE CASCADE,
   CONSTRAINT `contrat_ibfk_2` FOREIGN KEY (`client_id`) REFERENCES `client` (`client_id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -71,7 +97,7 @@ CREATE TABLE `contrat` (
 
 LOCK TABLES `contrat` WRITE;
 /*!40000 ALTER TABLE `contrat` DISABLE KEYS */;
-INSERT INTO `contrat` VALUES (1,'ABC123','jf415','2024-12-10','2024-12-15',15),(2,'DEF456','jf325','2024-12-11','2024-12-20',6);
+INSERT INTO `contrat` VALUES (10,'C54898','sf5894','2024-12-12','2025-01-11',30,12000.00),(11,'B5489','SH1256','2024-12-11','2024-12-19',8,2400.00),(12,'C54898','SH1256','2024-12-12','2024-12-21',9,3600.00),(13,'F6599','jf6898','2024-12-15','2024-12-20',5,1500.00),(14,'B5489','sf5894','2024-12-12','2024-12-29',17,5100.00),(15,'B5489','sf5894','2024-12-12','2024-12-28',16,4800.00);
 /*!40000 ALTER TABLE `contrat` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -101,7 +127,7 @@ CREATE TABLE `voiture` (
 
 LOCK TABLES `voiture` WRITE;
 /*!40000 ALTER TABLE `voiture` DISABLE KEYS */;
-INSERT INTO `voiture` VALUES ('ABC123','Toyota','Corolla',2020,'Essence','image1.png','En Parc',350.00),('DEF456','Renault','Clio',2019,'Diesel','image2.png','Sous Location',300.00);
+INSERT INTO `voiture` VALUES ('A25486','mercede','modern',2012,'Essence','../../public/assets/images/renault.png','Sous Location',230.00),('B5489','Renault','modern',2024,'Essence','../../public/assets/images/954-9541921_nouvelle-voiture-sans-retroviseur.png','En Parc',300.00),('C54898','Toyota','modern',2020,'Electrique','../../public/assets/images/toyota_PNG1952.png','En Parc',400.00),('F6599','Renault','clio',2021,'Essence','../../public/assets/images/pngimg.com - renault_PNG47.png','En Parc',300.00),('N45895','Renault','clio',2020,'Essence','../../public/assets/images/Renault-PNG-Free-File-Download.png','Sous Location',250.00);
 /*!40000 ALTER TABLE `voiture` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -118,50 +144,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-12-10 16:09:28
-
-
-
-
--- Création de database : 
--- CREATE TABLE voiture (matricule  varchar(250) PRIMARY KEY,marque VARCHAR(255) NOT NULL,modele varchar(255) not null ,Annee int NOT NULL,type_carburant ENUM('Diesel', 'Essence', 'Electrique', 'Hybride') NOT null,image_voiture VARCHAR(255),etat ENUM('En Parc', 'Sous Location') NOT NULL,prix_location DECIMAL(10, 2) NOT NULL);
-
--- CREATE TABLE client (client_id VARCHAR(100) PRIMARY KEY,nom VARCHAR(255) NOT NULL,prenom VARCHAR(255) NOT NULL,email VARCHAR(255) UNIQUE NOT NULL,telephone VARCHAR(15) NOT NULL);
-
--- CREATE TABLE contrat (contrat_id INT AUTO_INCREMENT PRIMARY KEY,voiture_id VARCHAR(250),client_id VARCHAR(100),date_debut DATE NOT NULL,date_fin DATE NOT NULL,Duree int NOT NULL,FOREIGN KEY (voiture_id) REFERENCES voiture(matricule) ON DELETE CASCADE,FOREIGN KEY (client_id) REFERENCES client(client_id) ON DELETE CASCADE);
-
--- Insertion a la base de dinnée : 
--- INSERT INTO voiture (matricule, marque, modele, Annee, type_carburant, image_voiture, etat, prix_location)
--- VALUES 
--- ('ABC123', 'Toyota', 'Corolla', 2020, 'Essence', 'image1.png', 'En Parc', 350),
--- ('DEF456', 'Renault', 'Clio', 2021, 'Diesel', 'image2.png', 'Sous Location', 300);
-
--- INSERT INTO client (client_id,nom, prenom, email, telephone)
--- VALUES 
--- ('jf415','Bouchta', 'Mohamed', 'Bouchtamohamed01@gmail.com', '0762389431'),
--- ('jf325','Hassani', 'lhbib', 'lahbib14@gmail.com.com', '0687654321');
-
--- INSERT INTO client (client_id,nom, prenom, email, telephone)
--- VALUES 
--- ('sf5894','benelaarbi', 'khadija', 'khadija2021@gmail.com', '0668382431');
-
--- INSERT INTO contrat (voiture_id, client_id, date_debut, date_fin, Duree)
--- VALUES 
--- ('ABC123','jf415', '2024-12-10', '2024-12-15', 15),
--- ('DEF456', 'jf325', '2024-12-11', '2024-12-20', 6);
-
--- Update quelque champs : 
--- update voiture set Annee = 2019 where matricule="DEF456";
--- update client set nom = baladen where client_id = sf5894;
-
-
--- select les tableux
--- select * from voiture
--- select * from client order by telephone DESC
--- select * from contrat,client,voiture where contrat.client_id=client.client_id and voiture.matricule = contrat.voiture_id;  
-
-
--- delete un client : 
--- delete from client where nom = "bouchta";
-
-
+-- Dump completed on 2024-12-25 10:23:53
